c120c182517bc9669bf0c9eaebed6fb9af8926b7
Edward-Watts <edwardwadjei@gmail.com>
2022-07-05 11:42:37 +0000

    feature(): add Poultrade and Papa's Kitchen to UI

--- src/utils/userInterface.js
+++ src/utils/userInterface.js
@@ -1,12 +1,42 @@
 let ui =[
     {
-        name: 'Zooish UI',
-        photo: 'userInterface/zooish-ui.jpg',
+        name: "Papa's",
+        photo: "userInterface/papa's.jpg",
+        info: null
+    },
+    {
+        name: "Papa's-share",
+        photo: "userInterface/papa's-share.jpg",
+        info: null
+    },
+    {
+        name: "Papa's-stacks",
+        photo: "userInterface/papa's-stack.jpg",
+        info: null
+    },
+    {
+        name: 'Poultrade',
+        photo: 'userInterface/poultrade.jpg',
+        info: null
+    },
+    {
+        name: 'Poultrade-share',
+        photo: 'userInterface/poultrade-share.jpg',
+        info: null
+    },
+    {
+        name: 'Poultrade-stacks',
+        photo: 'userInterface/poultrade-stacks.jpg',
         info: null
     },
     {
         name: 'Zooish',
         photo: 'userInterface/zooish-mobile.jpg',
+        info: null
+    },
+    {
+        name: 'Zooish UI',
+        photo: 'userInterface/zooish-ui.jpg',
         info: null
     },
     {
